d028932176e1962d9de87c9fb81ccf5ac2fccae9
Miso Tanaka <miso.develop@gmail.com>
2023-02-17 23:01:07 +0900

    [add] M5Unified対応に伴い新規追加

---
+++ template/m5atoms3-lite.ino
@@ -0,0 +1,20 @@
+#define M5ATOMS3_LITE_LED_ENABLE
+#include <OpnizM5Unified.h>
+#include <lib/WiFiConnector.h>
+
+WiFiConnector wifiConnector("<SSID>", "<PASSWORD>");
+Opniz::M5Unified* opniz = new Opniz::M5Unified("<ADDRESS>", <PORT>, "<ID>");
+
+void setup() {
+    initM5();
+    wifiConnector.setTimeoutCallback([]() { esp_restart(); });
+    wifiConnector.setConnectingSignal(blinkBlue);
+    wifiConnector.connect();
+    Serial.printf("opniz server address: %s\nopniz server port: %u\n\n", opniz->getAddress(), opniz->getPort());
+    opniz->connect();
+}
+
+void loop() {
+    opniz->loop();
+    wifiConnector.watch();
+}
---
+++ template/m5atoms3.ino
@@ -0,0 +1,19 @@
+#include <OpnizM5Unified.h>
+#include <lib/WiFiConnector.h>
+
+WiFiConnector wifiConnector("<SSID>", "<PASSWORD>");
+Opniz::M5Unified* opniz = new Opniz::M5Unified("<ADDRESS>", <PORT>, "<ID>");
+
+void setup() {
+    initM5();
+    wifiConnector.setTimeoutCallback([]() { esp_restart(); });
+    wifiConnector.setConnectingSignal(genBlinkDisplayMin("<SSID>", "<ADDRESS>", <PORT>, "<ID>"));
+    wifiConnector.connect();
+    Serial.printf("opniz server address: %s\nopniz server port: %u\n\n", opniz->getAddress(), opniz->getPort());
+    opniz->connect();
+}
+
+void loop() {
+    opniz->loop();
+    wifiConnector.watch();
+}
