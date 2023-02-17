fc39999f0ce4c28cf36a8f3f1525f600e65f4677
Miso Tanaka <miso.develop@gmail.com>
2023-02-17 22:58:32 +0900

    [update] deviceInfoListに対応ボード定義追加 [update] Arduino CLI、ArduinoJsonバージョンアップ [refact] defaultDevice, defaultPortの定義追加 [refact] deviceInfoEsp32, deviceInfoM5Unified定義追加

--- dist/config.js
+++ dist/config.js
@@ -1,43 +1,39 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.deviceInfoList = exports.dependenceLibraries = exports.core = exports.boardManager = exports.arduinoCliVersion = exports.arduinoDirsPath = exports.opnizHomePath = exports.arduinoConfigPath = exports.arduinoCliPath = exports.installPath = exports.downloadPath = exports.opnizRoot = void 0;
-const type_1 = require("./type");
-exports.opnizRoot = `.`;
-exports.downloadPath = `${exports.opnizRoot}/download`;
-exports.installPath = `${exports.opnizRoot}/arduino-cli`;
-exports.arduinoCliPath = `${exports.installPath}/arduino-cli`;
-exports.arduinoConfigPath = `${exports.opnizRoot}/arduino-cli.yaml`;
-exports.opnizHomePath = `${os.homedir().replace(/\\/g, "/")}/.opniz-cli`;
-exports.arduinoDirsPath = `${exports.opnizHomePath}/arduino-cli`;
-exports.arduinoCliVersion = "0.29.0";
-exports.boardManager = "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json";
-exports.core = "esp32:esp32@2.0.6";
-exports.dependenceLibraries = "ArduinoJson@6.17.3 WebSockets@2.3.6 ";
-exports.deviceInfoList = {
-    [type_1.Device.esp32]: {
-        fqbn: "esp32:esp32:pico32",
-        library: "",
-        repo: "https://github.com/miso-develop/opniz-arduino-esp32",
-        sketch: "esp32.ino",
-    },
-    [type_1.Device.m5atom]: {
-        fqbn: "esp32:esp32:m5stack-atom",
-        library: "M5Atom@0.1.0 FastLED ",
-        repo: "https://github.com/miso-develop/opniz-arduino-m5atom",
-        sketch: "m5atom.ino",
-    },
-    [type_1.Device.m5stickc]: {
-        fqbn: "esp32:esp32:m5stick-c",
-        library: "M5StickC@0.2.8 ",
-        // repo: "https://github.com/miso-develop/opniz-arduino-m5stickc", // MEMO: リリースしたら差し替え
-        repo: "https://github.com/miso-develop/opniz-arduino-esp32",
-        sketch: "m5stickc.ino",
-    },
-    [type_1.Device.m5stack]: {
-        fqbn: "esp32:esp32:m5stack-core-esp32",
-        library: "M5Stack@0.4.3 ",
-        // repo: "https://github.com/miso-develop/opniz-arduino-m5stack", // MEMO: リリースしたら差し替え
-        repo: "https://github.com/miso-develop/opniz-arduino-esp32",
-        sketch: "m5stack.ino",
-    },
+import "zx/globals";
+import { Device } from "./type.js";
+export const opnizRoot = `.`;
+export const downloadPath = `${opnizRoot}/download`;
+export const installPath = `${opnizRoot}/arduino-cli`;
+export const arduinoCliPath = `${installPath}/arduino-cli`;
+export const arduinoConfigPath = `${opnizRoot}/arduino-cli.yaml`;
+export const opnizHomePath = `${os.homedir().replace(/\\/g, "/")}/.opniz-cli`;
+export const arduinoDirsPath = `${opnizHomePath}/arduino-cli`;
+export const arduinoCliVersion = "0.30.0";
+export const boardManager = "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json";
+export const core = "esp32:esp32@2.0.6";
+export const dependenceLibraries = "ArduinoJson@6.20.0 WebSockets@2.3.6 ";
+export const defaultDevice = Device["m5atom"];
+export const defaultPort = 3000;
+const deviceInfoEsp32 = {
+    fqbn: "esp32:esp32:esp32",
+    library: "",
+    repo: "https://github.com/miso-develop/opniz-arduino-esp32",
+    sketch: "esp32.ino",
+};
+const deviceInfoM5Unified = {
+    fqbn: "",
+    library: "M5Unified@0.1.4 FastLED@3.5.0 ",
+    repo: "https://github.com/miso-develop/opniz-arduino-m5unified",
+};
+export const deviceInfoList = {
+    [Device["esp32"]]: Object.assign({}, deviceInfoEsp32),
+    [Device["esp32-pico"]]: Object.assign(Object.assign({}, deviceInfoEsp32), { fqbn: "esp32:esp32:pico32" }),
+    [Device["esp32-s3"]]: Object.assign(Object.assign({}, deviceInfoEsp32), { fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc" }),
+    [Device["m5stack"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:m5stack-core-esp32" }),
+    [Device["m5stack-core2"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:m5stack-core2", sketch: "m5stack.ino" }),
+    [Device["m5stickc"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:m5stick-c" }),
+    [Device["m5atom"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:m5stack-atom" }),
+    [Device["m5stamp-pico"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:m5stack-atom" }),
+    [Device["m5atoms3"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc" }),
+    [Device["m5atoms3-lite"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc" }),
+    [Device["m5stamp-s3"]]: Object.assign(Object.assign({}, deviceInfoM5Unified), { fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc" }),
 };
--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
-import { DeviceInfoList, Device } from "./type"
+import "zx/globals"
+import { Device, DeviceInfo, DeviceInfoList } from "./type.js"
 
 export const opnizRoot = `.`
 
@@ -13,41 +14,78 @@ export const arduinoDirsPath = `${opnizHomePath}/arduino-cli`
 
 
 
-export const arduinoCliVersion = "0.29.0"
+export const arduinoCliVersion = "0.30.0"
 
 
 
 export const boardManager = "https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json"
 export const core = "esp32:esp32@2.0.6"
-export const dependenceLibraries = "ArduinoJson@6.17.3 WebSockets@2.3.6 "
+export const dependenceLibraries = "ArduinoJson@6.20.0 WebSockets@2.3.6 "
 
 
+
+export const defaultDevice = Device["m5atom"]
+export const defaultPort = 3000
+
+
+
+const deviceInfoEsp32: DeviceInfo = {
+	fqbn: "esp32:esp32:esp32",
+	library: "",
+	repo: "https://github.com/miso-develop/opniz-arduino-esp32",
+	sketch: "esp32.ino",
+}
+
+const deviceInfoM5Unified: DeviceInfo = {
+	fqbn: "",
+	library: "M5Unified@0.1.4 FastLED@3.5.0 ",
+	repo: "https://github.com/miso-develop/opniz-arduino-m5unified",
+}
 
 export const deviceInfoList: DeviceInfoList = {
-	[Device.esp32]: {
+	[Device["esp32"]]: {
+		...deviceInfoEsp32,
+	},
+	[Device["esp32-pico"]]: {
+		...deviceInfoEsp32,
 		fqbn: "esp32:esp32:pico32",
-		library: "",
-		repo: "https://github.com/miso-develop/opniz-arduino-esp32",
-		sketch: "esp32.ino",
 	},
-	[Device.m5atom]: {
-		fqbn: "esp32:esp32:m5stack-atom",
-		library: "M5Atom@0.1.0 FastLED ",
-		repo: "https://github.com/miso-develop/opniz-arduino-m5atom",
-		sketch: "m5atom.ino",
+	[Device["esp32-s3"]]: {
+		...deviceInfoEsp32,
+		fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc",
 	},
-	[Device.m5stickc]: {
-		fqbn: "esp32:esp32:m5stick-c",
-		library: "M5StickC@0.2.8 ",
-		// repo: "https://github.com/miso-develop/opniz-arduino-m5stickc", // MEMO: リリースしたら差し替え
-		repo: "https://github.com/miso-develop/opniz-arduino-esp32",
-		sketch: "m5stickc.ino",
-	},
-	[Device.m5stack]: {
+	
+	[Device["m5stack"]]: {
+		...deviceInfoM5Unified,
 		fqbn: "esp32:esp32:m5stack-core-esp32",
-		library: "M5Stack@0.4.3 ",
-		// repo: "https://github.com/miso-develop/opniz-arduino-m5stack", // MEMO: リリースしたら差し替え
-		repo: "https://github.com/miso-develop/opniz-arduino-esp32",
+	},
+	[Device["m5stack-core2"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:m5stack-core2",
 		sketch: "m5stack.ino",
+	},
+	[Device["m5stickc"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:m5stick-c",
+	},
+	[Device["m5atom"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:m5stack-atom",
+	},
+	[Device["m5stamp-pico"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:m5stack-atom",
+	},
+	[Device["m5atoms3"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc",
+	},
+	[Device["m5atoms3-lite"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc",
+	},
+	[Device["m5stamp-s3"]]: {
+		...deviceInfoM5Unified,
+		fqbn: "esp32:esp32:esp32s3:CDCOnBoot=cdc",
 	},
 }
