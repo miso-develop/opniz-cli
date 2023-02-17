caf5852384ed47984a6257f4d2d651e61f02e3f8
Miso Tanaka <miso.develop@gmail.com>
2023-02-17 23:00:43 +0900

    [update] baudrateを9600→115200に変更 [update] opniz address, portをSerial出力

--- template/esp32.ino
+++ template/esp32.ino
@@ -5,9 +5,10 @@ WiFiConnector wifiConnector("<SSID>", "<PASSWORD>");
 Opniz::Esp32* opniz = new Opniz::Esp32("<ADDRESS>", <PORT>, "<ID>");
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     wifiConnector.setTimeoutCallback([]() { esp_restart(); });
     wifiConnector.connect();
+    Serial.printf("opniz server address: %s\nopniz server port: %u\n\n", opniz->getAddress(), opniz->getPort());
     opniz->connect();
 }
 
